2a99cdd3230df47a24ea5baa66af596715f6acea
Jorgewar13 <Jorgewar13@gmail.com>
2024-04-30 09:02:59 +0200

    oculus

---
+++ script.js
@@ -0,0 +1,43 @@
+AFRAME.registerComponent('collider-check', {
+    dependencies: ['raycaster'],
+  
+    init: function () {
+      console.log('ok collider check');
+      var debugtxt = document.querySelector('a-text');
+  
+      this.el.addEventListener('raycaster-intersection', this.handleIntersection.bind(this));
+      this.el.addEventListener('gripdown', this.handleGripDown.bind(this));
+      this.el.addEventListener('gripup', this.handleGripUp.bind(this));
+    },
+  
+    handleIntersection: function (e) {
+      this.selectedObj = e.detail.els[0];
+      debugtxt.setAttribute('value', 'Player hit something!');
+    },
+  
+    handleGripDown: function (e) {
+      this.grip = true;
+      debugtxt.setAttribute('value', 'Grip button pressed');
+    },
+  
+    handleGripUp: function (e) {
+      this.grip = false;
+      debugtxt.setAttribute('value', 'Grip button released');
+    },
+  
+    tick: function(){
+      if(!this.selectedObj || !this.grip) return;
+  
+      var raycast = this.el.getAttribute("raycaster").direction;
+      var pos = new THREE.Vector3(raycast.x, raycast.y, raycast.z);
+      pos.normalize();
+      pos.multiplyScalar(2);
+      this.el.object3D.localToWorld(pos);
+      this.selectedObj.object3D.position.set(pos.x, pos.y, pos.z);
+  
+      if (this.selectedObj.components["dynamic-body"]) {
+        this.selectedObj.components["dynamic-body"].syncToPhysics();
+      }
+    }
+  });
+  
